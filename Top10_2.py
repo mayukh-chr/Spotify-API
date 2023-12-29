0e4154fe7676d8645300a585767f57133b55b847
mayukh-chr <mayukhsaha.private@gmail.com>
2023-12-29 14:01:37 +0530

    fix artist name and initiate visualisation

--- Top10_2.py
+++ Top10_2.py
@@ -15,7 +15,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 def tenSavedTracks():
     
-    results = sp.current_user_saved_tracks(limit=10)
+    results = sp.current_user_saved_tracks(limit=1)
     for idx, item in enumerate(results['items']):
         track = item['track']
         print(idx+1, track['artists'][0]['name'], " - ", track['name'])
@@ -40,28 +40,30 @@ def printPlaylistTracks(playlist_id):
         
         track = item['track']
         album = track['album']
-        artist = album['artists']
+        artist = track['artists']
         artists_name = artist[0]['name']
         artist_id = artist[0]['id']
 
-        artistGenres(artist_id)
+        
+        print("genres: ", ', '.join(sp.artist(artist_id)['genres']))
         print("track name: ", track['name'])
         print("track ID: ", track['id'])
         print("artist ID: ", artist_id)
         
         print("Popularity: ", track['popularity'])
-        print("artist name: ", artists_name)                #this doesnt work for some reason, gonna check on it later
+        print("artist name: ", artists_name)                
         print("duration ms: ", track['duration_ms'])
         print("duration s: ", convertmstosec(track['duration_ms']))
         print("release year: ", album["release_date"][0:4])
         print("release month: ", album["release_date"][5:7])
-
+        
+        print('------------------------------------------------')
+        
         
         #print(f"{idx + 1}. {track['name']} by {track['artists'][0]['name']}")
         
         
         
-    print()
 
 def print_playlist_tracks_details(playlist_id):
     # Fetch playlist tracks
